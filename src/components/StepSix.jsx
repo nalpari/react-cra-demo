7b73876978dfdf1898f95d31e8d1104905c5dbd4
yoosangwook <yoo32767@gmail.com>
2024-08-20 17:11:00 +0900

    chore: Update StepSix component to use useCallback and useEffect

--- src/components/StepSix.jsx
+++ src/components/StepSix.jsx
@@ -1,7 +1,33 @@
+import { useCallback, useEffect, useState } from 'react'
+
 export default function StepSix() {
+  const [number, setNumber] = useState(0)
+  const [toggle, setToggle] = useState(true)
+
+  const handleClick = useCallback(() => {
+    console.log('current number:', number)
+  }, [number])
+
+  useEffect(() => {
+    console.log('handleClick 수정됨')
+  }, [handleClick])
+
+  useEffect(() => {
+    console.log('rerendering')
+  })
+
   return (
     <>
       <h1>useCallback</h1>
+      <div>
+        <input type="number" value={number} onChange={(e) => setNumber(e.target.value)} />
+      </div>
+      <div>
+        <button onClick={handleClick}>Click me</button>
+      </div>
+      <div>
+        <button onClick={() => setToggle(!toggle)}>{toggle ? `true` : `false`}</button>
+      </div>
     </>
   )
 }
