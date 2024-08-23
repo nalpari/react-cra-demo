8b0b91ca885c4a55672477ed9f694349e30c8856
yoosangwook <yoo32767@gmail.com>
2024-08-23 09:38:48 +0900

    chore: Add state management to ContextDemoChild component

--- src/components/demo/ContextDemoChild.jsx
+++ src/components/demo/ContextDemoChild.jsx
@@ -1,11 +1,15 @@
+import { useState } from 'react'
 import ContextDemoGrandChild from './ContextDemoGrandChild'
 import ContextDemoOther from './ContextDemoOther'
 
 export default function ContextDemoChild() {
   console.log('ContextDemoChild render')
+  const [state, setState] = useState({ name: 'John' })
   return (
     <>
       <h1>ContextDemoChild</h1>
+      <div>{state.name}</div>
+      <button onClick={() => setState({ name: 'Jane' })}>Change Name</button>
       <div>
         <ContextDemoOther />
         <ContextDemoGrandChild />
